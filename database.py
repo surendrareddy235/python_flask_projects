b30e42a53389e6b487930f74701f09bb49e2f081
surendrareddy235 <anjireddy7q@gmail.com>
2025-02-26 11:19:51 +0530

    updated commit

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
 # Configuring SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask.db'  # Creating a file called flask.db in the project
+db_path = os.path.join(os.path.abspath(os.getcwd()), 'flask.db')  # Store in project directory
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
